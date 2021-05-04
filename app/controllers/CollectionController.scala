58c00ac32992e6913623f69a594d109de4abb7e2
j000 <jarymut+github@gmail.com>
2021-05-04 23:48:54 +0200

    Add Collection model
    Still things to do...

--- app/controllers/CollectionController.scala
+++ app/controllers/CollectionController.scala
@@ -2,25 +2,62 @@ package controllers
 
 import javax.inject._
 import play.api.mvc.{Action, AnyContent}
-import play.api.mvc.{AbstractController, ControllerComponents}
+import play.api.mvc.{InjectedController, ControllerComponents}
+import play.api.libs.json._
+import play.api.mvc.Request
+import models.CollectionModel._
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class CollectionController @Inject()(val cc: ControllerComponents)
-extends AbstractController(cc)
+class CollectionController @Inject()(
+  val repo: CollectionRepository,
+)(
+  implicit ec: ExecutionContext
+) extends InjectedController
 {
-  def index(): Action[AnyContent] = Action {
-    Ok(s"collection list")
+  type CollectionDBO = repo.DBO
+
+  def index() = Action.async {
+    repo.index().map { collections =>
+      Ok(Json.toJson(collections))
+    }
   }
-  def read(id: Long): Action[AnyContent] = Action {
-    Ok(s"collection $id")
+  def create() = Action(parse.json).async { request =>
+    request.body.validate[Collection].fold(
+      problems => {
+        Future(BadRequest("Invalid json content"))
+      },
+      input => {
+        repo.create(input).map {
+          collection => Ok(Json.toJson(collection))
+        }
+      }
+    )
   }
-  def create(): Action[AnyContent] = Action {
-    Ok(s"collection created");
+  def read(id: Long) = Action.async {
+    repo.read(id).map {
+      case Some(collection) => Ok(Json.toJson(collection))
+      case _ => NotFound(Json.obj("error" -> "Not Found"))
+    }
   }
-  def update(id: Long): Action[AnyContent] = Action {
-    Ok(s"collection $id updated")
+  def update(id: Long) = Action(parse.json).async { implicit request =>
+    val collectionResult = request.body.validate[Collection]
+    collectionResult.fold(
+      errors => {
+        Future(BadRequest(Json.obj("error" -> "Invalid Json")))
+      },
+      collectionData => {
+        repo.update(id, collectionData).map {
+          case None => NotFound(Json.obj("error" -> "Not Found"))
+          case collection => Ok(Json.toJson(collection))
+        }
+      }
+    )
   }
-  def delete(id: Long): Action[AnyContent] = Action {
-    Ok(s"collection $id deleted")
+  def delete(id: Long) = Action.async {
+    repo.delete(id).map {
+      case 0 => NotFound(Json.obj("error" -> "Not Found"))
+      case _ => Ok(s"collection $id deleted")
+    }
   }
 }
---
+++ conf/evolutions/default/6.sql
@@ -0,0 +1,19 @@
+-- !Ups
+
+CREATE TABLE "Collections" (
+  "id" INTEGER PRIMARY KEY,
+  "name" VARCHAR NOT NULL
+);
+
+CREATE TABLE "Collections_helper" (
+  "id" INTEGER PRIMARY KEY,
+  "collection_id" INTEGER,
+  "book_id" INTEGER,
+
+  FOREIGN KEY(collection_id) REFERENCES Collections(id),
+  FOREIGN KEY(book_id) REFERENCES Books(id)
+);
+-- !Downs
+
+DROP TABLE "Collections_helper";
+DROP TABLE "Collections";
