d0004c0a09e718c166d70b975fe0ea66f7c0f686
j000 <jarymut+github@gmail.com>
2021-06-07 17:45:11 +0200

    Add reviews

--- front/src/components/BookList.jsx
+++ front/src/components/BookList.jsx
@@ -2,21 +2,34 @@ import React from 'react';
 import { useStore } from '~/src/contexts/StoreContext';
 
 export default () => {
-	const [store, storeDispatch] = useStore();
-	const { books, authors, genres } = store;
+	const [{
+		books,
+		authors,
+		genres,
+		reviews,
+	}, storeDispatch] = useStore();
 
 	return (<ul>
 		{
 			Object.values(books).map((book) => (
 				<li key={ book.id }>
-					{
-						book.id
-					}. {
-						book.title
-					} by {
-						authors?.[book.author_id]?.name || <i>author #{book.author_id}</i>
-					} - {
+					<q>
+						{ book.title }
+					</q> by <i>{
+						authors?.[book.author_id]?.name || <>author #{book.author_id}</>
+					}</i> - {
 						genres?.[book.genre_id]?.name || <i>genre #{book.genre_id}</i>
+					}
+					{
+						reviews?.[book.id]?.length && (
+							<ul>
+								{reviews?.[book.id]?.map((review) => (
+									<li key={ review.id }>
+										{ review.content }
+									</li>
+								))}
+							</ul>
+						)
 					}
 				</li>
 			))
